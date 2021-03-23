73376dba6d3b6ee9999f754a625498313311228e
Sergen Kahraman <sergen_kahraman_@hotmail.com>
2021-03-23 11:36:31 +0300

    vue client tarafı başlatıldı

---
+++ src/client/puf.client/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="nav" class="navbar">
+    <router-link to="/" class=" nav-item ">Home</router-link> |
+    <router-link to="/about" class=" nav-item">About</router-link>
+  </div>
+  <router-view/>
+</template>
+
+<style>
+
+</style>
---
+++ src/client/puf.client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <SideBar msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SideBar from '@/components/SideBar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    SideBar
+  }
+}
+</script>
